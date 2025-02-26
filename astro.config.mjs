e71e6523fa7d1d3adecac96dd0bce800c7de74a6
JonnyWapo <jonathanalexi503y507@gmail.com>
2025-02-25 23:54:24 -0500

    Upadte home

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
+import image from "@astrojs/image"; // Importa el soporte de imágeness
 
 // If you are not going to use vercel analytics you can delete the analytics
 // If you want to remove vercel from your project you can do npm uninstall vercel
@@ -9,6 +10,7 @@ export default defineConfig({
     webAnalytics: {
       enabled: true,
     },
+  integrations: [image()], // Habilita la integración de imágenes
   }),
 });
 
